e6a1709d5b10ab5092c382d3af8dd93ed35ad04b
Michael Debono Mrđen <michael@debonomren.eu>
2020-10-03 18:33:59 +0200

    Added model

--- cram/admin.py
+++ cram/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Page, Subscription, Cache
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Page)
+admin.site.register(Subscription)
+admin.site.register(Cache)
--- cram/models.py
+++ cram/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField()
+    enabled = models.BooleanField(default=False)
+    def __str__(self):
+        return self.email
+
+class Page(models.Model):
+    country = models.CharField(max_length=2)
+    url = models.URLField()
+    xpath_country = models.CharField(max_length=1000, blank=True)
+    regex_country = models.CharField(max_length=1000, blank=True)
+    xpath_time = models.CharField(max_length=1000, blank=True)
+    regex_time = models.CharField(max_length=1000, blank=True)
+    def __str__(self):
+        return '{}: {}'.format(self.country, self.url)
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+    country = models.CharField('country to monitor', max_length=200)
+    enabled = models.BooleanField(default=False)
+    def __str__(self):
+        return 'user={} page={} country={}'.format(self.user.pk, self.page.pk, self.country)
+
+class Cache(models.Model):
+    timestamp = models.DateTimeField()
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+    html = models.TextField()
+    def __str__(self):
+        return 'page={} timestamp={}'.format(self.page, self.timestamp)
